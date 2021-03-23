17c142d3a5b8a13f54075e1e1ca7d21c27f7cedd
Mark Rosenberg <mrosenberg@prscholas.org>
2021-03-23 11:01:13 -0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius, area;
+            Console.Write("Please enter a radius: ");
+            string sRadius = Console.ReadLine();
+            bool success = double.TryParse(sRadius, out radius);
+            if(!success)
+            {
+                Console.Write("That wasn't a real number ");
+                return;
+            }
+            //radius = double.Parse(sRadius);
+            area = radius * radius * Math.PI;
+            Console.WriteLine($"The area of a circle of radius {radius:F3} is {area:F3} ");
+            Console.ReadKey();
+            
+        }
+    }
+}
