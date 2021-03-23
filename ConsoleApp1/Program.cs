b06bebfe9294d0322e66171fce15b6ea7ea3b34d
Mark Rosenberg <mrosenberg@prscholas.org>
2021-03-23 16:45:37 -0500

    In class Excersize from Mark

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,33 +7,9 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            bool stop = false;
-            while (!stop)
-            {
-                Console.Write("Please enter a number to calculate square root: ");
-                string sSquare = Console.ReadLine();
-                if (sSquare == "q")
-                {
-                    stop = true;
-                    continue;
-                }
-                if (!double.TryParse(sSquare, out double square))
-                {
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.Write("That wasn't a real number try again ");
-                    
-                    continue;
-                }
-                if (square < 0)
-                {
-                    Console.BackgroundColor = ConsoleColor.Yellow;
-                    Console.Write("Don't use negative numbers ");
-                    continue;
-                }
-                double area = Math.Sqrt(square);
-                Console.WriteLine($"The square root of {square:F3} is {area:F3} ");
-
-            }
+           Console.Write("Please enter some text: ");
+           string sSquare = Console.ReadLine();
+           Console.WriteLine($"You typed {sSquare}");
         }
     }
 }
