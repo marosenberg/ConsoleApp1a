9edd2d650090e4eb5463c34c574fbf76133980d6
Mark Rosenberg <mrosenberg@prscholas.org>
2021-03-26 16:06:01 -0500

    Class demo

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        private int myVar;
+        private string firstName;
+        private string lastName;
+
+        public string FullName
+        {
+            get
+            {
+                return firstName + " " + lastName;
+            }
+        }
+        public int MyProperty
+        {
+            get { return myVar; }
+            set 
+            { 
+                if (value > 5 || value < 1)
+                {
+                    throw new Exception();
+                }
+                myVar = value; 
+            }
+        }
+
+    }
+}
